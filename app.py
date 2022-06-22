9e2eca2aca02caf0c9c5dbb11379cd882b526227
Calum Mackervoy <c.mackervoy@gmail.com>
2022-06-22 22:02:48 +0300

    POST and GET world data from MongoDB

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import json
 from flask import Flask, request, jsonify
 from rdflib import Graph
 from urllib.parse import unquote_plus
+from pymongo import MongoClient
+from bson import json_util
+
+# config
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.simpolis
 
 @app.route("/")
 def main():
@@ -12,25 +19,20 @@ def main():
 @app.route("/world/", methods=['POST', 'GET'])
 def world():
     if request.method == 'POST':
-        g = Graph().parse(data=json.dumps(request.get_json()), format='json-ld')
-        
-        return jsonify(g.serialize(format='json-ld')), 200
+        # g = Graph().parse(data=json.dumps(request.get_json()), format='json-ld')
+        # jsonld = json.loads(g.serialize(format='json-ld'))
+        jsonld = request.get_json()
 
-    data = {
-        "@context": {
-            "hasTileMap": {
-                "@id": "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudworld.ttl#hasTileMap",
-                "@container": "@list"
-            }
-        },
-        "@id": "http://example.org/people#joebob",
-        "https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudworld.ttl#Region": {
-            "http://xmlns.com/foaf/0.1/name": "Attica",
-            "hasTileMap": [
-                [0,0,0],
-                [0,0,0]
-            ]
-        }
-    }
+        # assign the graph an id
+        if "@id" not in jsonld:
+            jsonld["@id"] = "http://localhost:5000/world/"
 
-    return jsonify(data), 200, {'Content-Type': 'application/ld+json'}
+        # creates or updates the world graph
+        db.world.find_one_and_replace(
+            {"@id": jsonld["@id"]},
+            jsonld,
+            upsert=True
+        )
+        return jsonify(jsonld), 200
+
+    return jsonify(json.loads(json_util.dumps(db.world.find_one()))), 200, {'Content-Type': 'application/ld+json'}
